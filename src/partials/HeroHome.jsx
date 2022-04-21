13a81aa02007c29967db2e6e51a6749e5144184e
JHLee <tommykim4763@gmail.com>
2022-04-21 14:00:45 +0900

    수정

--- src/partials/HeroHome.jsx
+++ src/partials/HeroHome.jsx
@@ -36,7 +36,7 @@ function HeroHome() {
             <h1
               className="text-5xl md:text-6xl font-extrabold leading-tighter tracking-tighter mb-4"
               data-aos="zoom-y-out">
-              Welcome{" "}
+              Welcome to{" "}
               <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-teal-400">
                 JHLab
               </span>
